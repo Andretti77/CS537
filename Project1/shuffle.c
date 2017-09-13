4348e6f6952deb2a936cc04704ae172299dbdf2f
Andrew Petti <petti@royal-30.cs.wisc.edu>
2017-09-12 21:59:50 -0500

    Have to use fread. Making things tricky.

--- Project1/shuffle.c
+++ Project1/shuffle.c
@@ -3,43 +3,79 @@
 #include <string.h>
 int main(int argc, char *argv[]){
 
-    FILE *in_file;
+    FILE* in_file;
+    FILE* out_file;
 
-    in_file = fopen(argv[1],"r");
+    in_file = fopen(argv[2],"r");
+    out_file = fopen(argv[4], "w");
 
-    char* word = NULL;
-
-    char* words[300];
-
-    int line_length;
-    unsigned long int len;
+    char  word[512];
+    char  words[300];
+    char* ptrs_to_words[300];
+    //char* last_word;
+    //int line_length;
+   // unsigned long int len;
     int num_lines;
-    int i;
+    int char_num;
+    int j;
+    //int k;
+    num_lines =0;
+    char_num = 0;
+    	    
 
-    num_lines = 0;
-
-
+    
+	   
+    fread(words, sizeof(char), 512, in_file);	    
 	
+    for(j=0; j<sizeof(words); j++){
 
-    while((line_length = getline(&word, &len, in_file)) != -1){
-	
-	    words[num_lines] = strdup(word);
-	    num_lines++;
+	if(words[j] !='\n'){
+	 
+	 	
+	 word[char_num] = words[j];
+	 char_num++;
 
-     
+
+
+	}else{
+	 	
+	 word[char_num] = words[j];
+
+	 //printf("%s", word);	 
+    	 ptrs_to_words[num_lines] = strdup(word);
+	 //printf("%s", ptrs_to_words[num_lines]);
+	 char_num = 0;
+	 num_lines++;
+
+	}  
 
     }
 
+   
     
-   for(i=0; i<num_lines; i++){
-	free(words[i]);
-	
 
-   }
+   
+
+  
+
+
+ 	 
+  
+   //fwrite(ptrs_to_words[0], sizeof(char)*512, 1, out_file);
+   for(j=0; j<num_lines/2; j++){
+	printf("%s", ptrs_to_words[0]); 
+
+	fwrite(ptrs_to_words[j],sizeof(char),512, out_file);
+	fwrite(ptrs_to_words[num_lines-j-1],sizeof(char), 512 , out_file);	
+
+
+   }	
+
+ //  }
 
     
-    free(word);
     fclose(in_file);
+    fclose(out_file);
     return 0;
 
 }

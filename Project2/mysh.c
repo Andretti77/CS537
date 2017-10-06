7b5c003f5539e8a3d785887bf75e50f2aba9c171
Andrew Petti <petti@emperor-04.cs.wisc.edu>
2017-10-06 14:48:21 -0500

    redirect does not work pls help

--- Project2/mysh.c
+++ Project2/mysh.c
@@ -5,6 +5,8 @@
 #include <libgen.h>
 #include <errno.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 int main(){
     int command_num = 1;
@@ -12,14 +14,14 @@ int main(){
         char* input = (char*) malloc(sizeof(char)*128);
         printf("mysh (%d)>", command_num);
         fgets(input, 128*sizeof(char), stdin);
-        char* command = strtok(input, " \n");
+        char* command = strtok(input, " \n\t");
        
 
 
         if(strcmp(command, "exit") == 0){
                 exit(0);
         }else if(strcmp(command, "cd") == 0){
-                char* directory = strtok(NULL, " \n");
+                char* directory = strtok(NULL, " \n\t");
                 if(directory == NULL){
                     directory = getenv("HOME");
                     chdir(directory);
@@ -46,23 +48,45 @@ int main(){
         }else{
             char** arguments = (char**) malloc(64*sizeof(char*));
             int i;
+            int fd;
+            int out_redirection=0;
+            char* outfile;
+            printf("HERE\n");
             arguments[0] = command;
             for(i=1; i<256; i++){
-
                 char* arg = (char*) malloc(128);
-                arg = strtok(NULL, " \n");
-                arguments[i]= arg;
-                if(arg == NULL){
-                    break;
+                arg = strtok(NULL, " \n\t");
+                if(strcmp(arg, ">") == 0){
+                    out_redirection = 1;
+                    outfile = strtok(NULL, " \n\t");
+                    fd = open(outfile, O_WRONLY|O_CREAT|O_TRUNC);
+                    
+
+
+
+                }else{
+                    
+                    arguments[i]= arg;
+                
+                    if(arg == NULL){
+                        break;
+                    }
+
                 }
                 
             }
-       
+            printf("HERE\n"); 
             int pid;
 
             pid= fork();
-
-            if(pid == 0){   
+            
+            if(pid == 0){
+                if(out_redirection){
+                    dup2(fd, 1);
+                    printf("HERE\n");
+                    close(fd);
+                }
+                printf("BEFORE EXECVP\n");   
                 execvp(command,arguments);
             }else{
                 waitpid(pid, NULL, 0);

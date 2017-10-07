937c68748023cbb957ea5eec2c59affe0eacc477
AustinKlisch <aklisch@wisc.edu>
2017-10-07 11:34:39 -0500

    Added functions, trying to figure out redirection atm and struggling

--- Project2/mysh.c
+++ Project2/mysh.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -8,6 +9,54 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
+
+
+char* cdfunc(){
+    char* directory = strtok(NULL, " \n\t");
+    if(directory == NULL){
+        directory = getenv("HOME");
+        chdir(directory);
+        free(directory);
+        
+    }else{
+        char* path = (char*) malloc(strlen(directory));
+        strcat(path, "./");
+        strcat(path, directory);
+        chdir(path);
+        free(path);
+    }
+    char* output = "";
+    return output;
+}
+
+char* pwdfunc(){
+    char* directory = (char*)malloc(sizeof(char)*124);
+    getcwd(directory, 124);
+    return directory;
+}
+
+void execfunc(char* command){
+    char** arguments = (char**) malloc(64*sizeof(char*));
+    int i;
+    arguments[0] = command;
+    for(i=1; i<256; i++){
+        
+        char* arg = (char*) malloc(128);
+        arg = strtok(NULL, " \n");
+        arguments[i]= arg;
+        if(arg == NULL){
+            break;
+        }
+    }
+    int pid;
+    pid= fork();
+    if(pid == 0){
+        execvp(command,arguments);
+    }else{
+        waitpid(pid, NULL, 0);
+    }
+}
+
 int main(){
     int command_num = 1;
     while(1){
@@ -15,87 +64,24 @@ int main(){
         printf("mysh (%d)>", command_num);
         fgets(input, 128*sizeof(char), stdin);
         char* command = strtok(input, " \n\t");
-       
-
-
+        char* output = (char*)malloc(sizeof(char)*124);
         if(strcmp(command, "exit") == 0){
-                exit(0);
+            exit(0);
         }else if(strcmp(command, "cd") == 0){
-                char* directory = strtok(NULL, " \n\t");
-                if(directory == NULL){
-                    directory = getenv("HOME");
-                    chdir(directory);
-
-                }else{
-                    char* path = (char*) malloc(strlen(directory));
-                    strcat(path, "./");
-                    strcat(path, directory);
-                    chdir(path);
-                    free(path);
-                }   
+            output = cdfunc();
 
         }else if(strcmp(command, "pwd") == 0){
-                char* directory = (char*)malloc(sizeof(char)*124);
-                getcwd(directory, 124);
-                printf("%s\n", directory);
-                free(directory);
-
+            output = pwdfunc();
         }else{
-            char** arguments = (char**) malloc(64*sizeof(char*));
-            int i;
-            int fd;
-            int out_redirection=0;
-            char* outfile;
-            printf("HERE\n");
-            arguments[0] = command;
-            for(i=1; i<256; i++){
-                char* arg = (char*) malloc(128);
-                arg = strtok(NULL, " \n\t");
-                if(strcmp(arg, ">") == 0){
-                    out_redirection = 1;
-                    outfile = strtok(NULL, " \n\t");
-                    fd = open(outfile, O_WRONLY|O_CREAT|O_TRUNC);
-                    
-
-
-
-                }else{
-                    
-                    arguments[i]= arg;
-                
-                    if(arg == NULL){
-                        break;
-                    }
-
-                }
-                
-            }
-            printf("HERE\n"); 
-            int pid;
-
-            pid= fork();
-            
-            if(pid == 0){
-                if(out_redirection){
-                    dup2(fd, 1);
-                    printf("HERE\n");
-                    close(fd);
-                }
-                printf("BEFORE EXECVP\n");   
-                execvp(command,arguments);
-            }else{
-                waitpid(pid, NULL, 0);
-            }
-
-
+            execfunc(command);
         }
-
+        if(strcmp(output, "") != 0){
+            printf("%s\n", output);
+        }
         fflush(stdout);
         command_num++;
-    
-
-
         free(input);
 
     }
 }
+

cf897ead893751f7777155256a1c4d270f80c08c
Andrew Petti <petti@royal-25.cs.wisc.edu>
2017-10-31 14:52:58 -0500

    started the project still need to do exiting stuff

--- Project3/shm_client.c
+++ Project3/shm_client.c
@@ -4,10 +4,23 @@
 #include <unistd.h>
 #include <pthread.h>
 #include <sys/mman.h>
+#include <time.h>
+#include <string.h>
 // ADD NECESSARY HEADERS
-
+#define SHMNAME "petti_klisch"
+#define PAGESIZE 4096
+#define MAXCLIENTS 64
 // Mutex variables
 pthread_mutex_t* mutex;
+
+
+typedef struct{
+    int pid;
+    char birth[25];
+    char clientString[10];
+    int elapsed_sec;
+    double elapsed_msec;
+}stats_t;
 
 void exit_handler(int sig) {
     // ADD
@@ -16,7 +29,7 @@ void exit_handler(int sig) {
 	pthread_mutex_lock(mutex);
 
     // Client leaving; needs to reset its segment   
-
+    
 	pthread_mutex_unlock(mutex);
 	// critical section ends
 
@@ -25,22 +38,55 @@ void exit_handler(int sig) {
 
 int main(int argc, char *argv[]) {
 	// ADD    
-	
+	int fd = shm_open(SHMNAME, O_RDWR, 0660);
+    if(fd == -1)
+       exit(1); 
+    void*  shm_ptr = mmap(NULL, PAGESIZE, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
+    if(shm_ptr == NULL)
+        exit(1);
+
+    //time variables
+    time_t tictoc;
+    time(&tictoc);
+    struct tm* now;
+    char time[25];
+    now = localtime(&tictoc);
+
+    strftime(time, 25, "%c", now);
     // critical section begins
+    mutex = (pthread_mutex_t*)shm_ptr;
     pthread_mutex_lock(mutex);
-        
+    stats_t* currClient = NULL; 
+
 	// client updates available segment
-		
+    for(int i = 1; i<MAXCLIENTS; i++){
+        currClient = (stats_t *)shm_ptr+(i*64);
+        if(currClient -> pid == -1)
+            break;
+    }
+	currClient -> pid = getpid();
+    strcpy(currClient -> birth, time);
+    currClient -> elapsed_sec = 1;
+    currClient -> elapsed_msec = 0;
+    strcpy(currClient -> clientString ,argv[1]);
 	pthread_mutex_unlock(mutex);
     // critical section ends
         
 	while (1) {
         
 		// ADD
-        
+		// Print active clients
+        printf("Active clients: ");
+        for(int i = 1; i<MAXCLIENTS; i++){
+            pthread_mutex_lock(mutex);
+            currClient = (stats_t*) shm_ptr+(i*64);
+            if(currClient->pid != -1)
+                printf("%d", currClient->pid);
+            pthread_mutex_unlock(mutex);
+        } 
+        printf("\n");
         sleep(1);
 
-		// Print active clients
     }
 
     return 0;
--- Project3/shm_server.c
+++ Project3/shm_server.c
@@ -5,11 +5,23 @@
 #include <pthread.h>
 #include <sys/mman.h>
 // ADD NECESSARY HEADERS
-
+#define SHM_NAME "petti_klisch"
+#define PAGESIZE 4096
+#define MAXCLIENTS 64
 // Mutex variables
 pthread_mutex_t* mutex;
 pthread_mutexattr_t mutexAttribute;
 
+typedef struct { 
+    int pid;
+    char birth[25];
+    char clientString[10];
+    int elapsed_sec;
+    double elapsed_msec;
+} stats_t;
+
+
+int numClients=0;
 void exit_handler(int sig) 
 {
     // ADD
@@ -22,17 +34,44 @@ int main(int argc, char *argv[])
     // ADD
 	
 	// Creating a new shared memory segment
-	int fd_shm = shm_open(SHM_NAME, O_RDWR | O_CREAT, 0660);	
-        
+	int fd = shm_open(SHM_NAME, O_RDWR | O_CREAT, 0660);	
+    if(fd == -1 )
+        exit(1);
+    ftruncate(fd, PAGESIZE);
+    void * shm_ptr =  mmap(NULL, PAGESIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd ,0);
+    if(shm_ptr == NULL)
+        exit(1);
+
+    mutex = (pthread_mutex_t*) shm_ptr;
+
     // Initializing mutex
 	pthread_mutexattr_init(&mutexAttribute);
 	pthread_mutexattr_setpshared(&mutexAttribute, PTHREAD_PROCESS_SHARED);
-	pthread_mutex_init(mutex, &mutexAttribute);
+    pthread_mutex_init(mutex, &mutexAttribute);
+   
+    int i;
+    //setting all pids to -1 to see where to add new client data
+    for(i =1; i<MAXCLIENTS; i++){
+        stats_t* currClient = (stats_t*) shm_ptr+(i*64);    
+        currClient->pid = -1;
+
+    }
 
     while (1) 
 	{
 		// ADD
+        for(i = 1; i<MAXCLIENTS; i++){
 
+            pthread_mutex_lock(mutex);
+            stats_t* currClient = (stats_t*)shm_ptr+(i*64);
+
+            if(currClient->pid != -1)
+                printf("%d, pid : %d, birth : %s, elapsed : %d s %f ms, %s\n",
+                        i, currClient->pid, currClient->birth, currClient->elapsed_sec, currClient->elapsed_msec, currClient->clientString);
+
+            pthread_mutex_unlock(mutex);
+        }
+        
         sleep(1);
     }
 

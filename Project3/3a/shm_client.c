94f569ee60c0d5bb7068242f97665d57e483384d
Andrew Petti <petti@snares-08.cs.wisc.edu>
2017-11-05 10:30:34 -0600

    commented code and passes all tests

--- Project3/3a/shm_client.c
+++ Project3/3a/shm_client.c
@@ -1,3 +1,4 @@
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
@@ -5,129 +6,141 @@
 #include <pthread.h>
 #include <sys/mman.h>
 #include <time.h>
-#include <sys/time.h>
-#include <string.h>
 #include <signal.h>
-// ADD NECESSARY HEADERS
-#define SHMNAME "petti_klisch"
-#define PAGESIZE 4096
-#define MAXCLIENTS 64
-// Mutex variables
-pthread_mutex_t* mutex;
+#include <string.h>
+#include <sys/time.h>
 
-
-typedef struct{
-    char valid;
-    int pid;
-    char birth[25];
-    char clientString[10];
-    int elapsed_sec;
-    double elapsed_msec;
+typedef struct {
+	int pid;
+	char birth[25];
+	char clientString[10];
+	int elapsed_sec;
+	double elapsed_msec;
 } stats_t;
 
-void* ptr;
+// Mutex variables
+pthread_mutex_t* mutex;
+//pointer to the shared memory
+void * ptr;
+//this processes client
 stats_t* thisClient;
 
 void exit_handler(int sig) {
-    // ADD
 
-    // critical section begins
+	// critical section begins
 	pthread_mutex_lock(mutex);
 
-    // Client leaving; needs to reset its segment   
-    thisClient->valid = 0;
+	// Client leaving; needs to reset its segment
+	thisClient->pid = -1;
+
 	pthread_mutex_unlock(mutex);
 	// critical section ends
 
-    exit(0);
+	exit(0);
 }
 
 int main(int argc, char *argv[]) {
-	// ADD    
-    
-    if(argc!=2)
-        exit(1);
 
-    struct sigaction act;
-    memset(&act, 0 , sizeof(act));
-    act.sa_handler = exit_handler;
-    act.sa_flags = 0;
+	if (argc != 2)
+		exit(1);
 
-    if(sigaction(SIGINT, &act, NULL) < 0)
-        return 1;
 
-    if(sigaction(SIGTERM, &act, NULL) <0)
-        return 1;
-    
+    //setting up sighandler
+	struct sigaction act;
+	memset(&act, 0, sizeof(act));
+	act.sa_handler = exit_handler;
+	act.sa_flags = 0;
+	if(sigaction(SIGINT, &act, NULL) != 0)
+		exit(1);
+	if(sigaction(SIGTERM, &act, NULL) != 0)
+		exit(1);
 
-    int fd_shm = shm_open(SHMNAME, O_RDWR, 0660);
-    if(fd_shm == -1)
-        exit(1);
+	
+    //file descriptor for the shared memory
+	int fd_shm = shm_open("petti_klisch", O_RDWR, 0660);
+	if (fd_shm == -1)
+		exit(1);
 
-    ptr = mmap(NULL , 4096, PROT_WRITE | PROT_READ, MAP_SHARED, fd_shm, 0);
-    if(ptr == (void*) -1)
-        exit(1);
+    //setting up the pointer
+	ptr = mmap(NULL, 4096, PROT_WRITE | PROT_READ, MAP_SHARED, fd_shm, 0);
+	if (ptr == (void *)-1)
+		exit(1);
 
-    mutex = (pthread_mutex_t*) ptr;
+    //lock is in the first entry in shared memory
+	mutex = (pthread_mutex_t*) ptr;
 
-    time_t tic;
-    struct tm* now;
-    char created[25];
-    time(&tic);    
-    now = localtime(&tic);
-    strftime(created, 25, "%c", now);
 
-    struct timeval time_start, time_curr;
-    gettimeofday(&time_start, NULL);
+    //setting up time variables for birth
+	time_t t;
+	struct tm *now;
+	char time_buffer[25];
+	time (&t);
+	now = localtime(&t);
+	strftime(time_buffer, 25, "%c", now);
 
-    // critical section begins
-    pthread_mutex_lock(mutex);
-        
+    //time variables for elapsed time 
+	struct timeval start_time;
+	struct timeval curr_time;
+	gettimeofday(&start_time, NULL);
+    int i;
+	// critical section begins
+	pthread_mutex_lock(mutex);
+
+    //finding the first slot available in the shared memory
+    //and adding the client to this
+    //first slot is indicated by a pid of -1
+    //
+    //starting at 1 to skip over the lock
+	for (i = 1; i < 64; i++){
+		thisClient = (stats_t *) (ptr + 64 * i);
+		if (thisClient->pid == -1){
+            //found the first available slot
+			thisClient->pid = getpid();
+			strcpy(thisClient->birth, time_buffer);
+			strcpy(thisClient->clientString, argv[1]);
+			thisClient->elapsed_sec = 0;
+			thisClient->elapsed_msec = 0;
+            //do not want to continue in loop so break
+			break;
+		}
+
+	}
 	// client updates available segment
-    for(int i =1; i<MAXCLIENTS; i++){
 
-        thisClient = (stats_t*) (ptr+64*i);
-        if(thisClient -> valid == 0){
-            thisClient -> pid = getpid();
-            strcpy(thisClient->birth, created);
-            strcpy(thisClient->clientString, argv[1]);
-            thisClient->elapsed_sec = 0;
-            thisClient->elapsed_msec = 0;
-            thisClient->valid = 1;
-            break;
-        }
-        
-        if(i == 63)
-            exit(1);
-    } 
-		
 	pthread_mutex_unlock(mutex);
-    // critical section ends
-        
+	// critical section ends
+
+    //max clients has been reached so no more clients allowed so exit
+    if(i > 63)
+        exit(1);
+
+
 	while (1) {
-        
-		// ADD
-        gettimeofday(&time_curr, NULL);
-  //      pthread_mutex_lock(mutex);
-
-        thisClient->elapsed_sec = time_curr.tv_sec - time_start.tv_sec;
-        thisClient->elapsed_msec = (time_curr.tv_usec - time_start.tv_usec)/1000.0;
-        
-//        pthread_mutex_unlock(mutex);
-
-        sleep(1);
+        //updating elapesed time for this process
+        gettimeofday(&curr_time, NULL);
+		thisClient->elapsed_sec = (curr_time.tv_sec - start_time.tv_sec);
+		thisClient->elapsed_msec = (curr_time.tv_usec - start_time.tv_usec) / 100.0;
+		sleep(1);
 
 		// Print active clients
-        printf("Active clients: ");
-        for(int i = 1; i<MAXCLIENTS; i++){
+		printf("Active clients : ");
+		// critical section begins
 
-            stats_t* temp = (stats_t*) (ptr+64*i);
-            if(temp->valid == 1)
-                printf("%d", temp->pid);
+		pthread_mutex_lock(mutex);
+		for (int i = 1; i < 64; i++){
+			stats_t* currClient = (stats_t *) (ptr + 64 * i);
+			if(currClient->pid != -1){
+				printf("%d ", currClient->pid);
+				
+			}
 
-        }
-        printf("\n");
-    }
+			//if (i >= 63)
+				//printf("\n");
+		}
+		printf("\n");
+		pthread_mutex_unlock(mutex);
+		// critical section ends
+	}
 
-    return 0;
+	return 0;
 }

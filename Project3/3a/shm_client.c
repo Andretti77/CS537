145011c7a02927303b406f53ad372f56c0d848d1
Austin Klisch <klisch@snares-10.cs.wisc.edu>
2017-11-05 13:59:03 -0600

    updated a few things, all tests are passing

--- Project3/3a/shm_client.c
+++ Project3/3a/shm_client.c
@@ -1,4 +1,3 @@
-#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
@@ -10,7 +9,12 @@
 #include <string.h>
 #include <sys/time.h>
 
+#define SHMNAME "petti_klisch"
+#define PAGESIZE 4096
+#define MAXCLIENTS 64
+
 typedef struct {
+	char valid;
 	int pid;
 	char birth[25];
 	char clientString[10];
@@ -25,119 +29,116 @@ void * ptr;
 //this processes client
 stats_t* thisClient;
 
-void exit_handler(int sig) {
-
+void exit_handler(int sig)
+{
 	// critical section begins
 	pthread_mutex_lock(mutex);
 
 	// Client leaving; needs to reset its segment
-	thisClient->pid = -1;
+	thisClient->valid = 0;
 
-	pthread_mutex_unlock(mutex);
 	// critical section ends
-
+	pthread_mutex_unlock(mutex);
 	exit(0);
 }
 
-int main(int argc, char *argv[]) {
-
+int main(int argc, char *argv[]) 
+{
 	if (argc != 2)
 		exit(1);
 
-
-    //setting up sighandler
+	//setting up sighandler
 	struct sigaction act;
 	memset(&act, 0, sizeof(act));
 	act.sa_handler = exit_handler;
 	act.sa_flags = 0;
-	if(sigaction(SIGINT, &act, NULL) != 0)
-		exit(1);
+	
 	if(sigaction(SIGTERM, &act, NULL) != 0)
 		exit(1);
 
-	
-    //file descriptor for the shared memory
-	int fd_shm = shm_open("petti_klisch", O_RDWR, 0660);
-	if (fd_shm == -1)
+	if(sigaction(SIGINT, &act, NULL) != 0)
 		exit(1);
 
-    //setting up the pointer
-	ptr = mmap(NULL, 4096, PROT_WRITE | PROT_READ, MAP_SHARED, fd_shm, 0);
-	if (ptr == (void *)-1)
+
+	//file descriptor for the shared memory
+	int fd_shm = shm_open(SHMNAME, O_RDWR, 0660);
+	if(fd_shm == -1)
 		exit(1);
 
-    //lock is in the first entry in shared memory
+  //setting up the pointer
+	ptr = mmap(NULL, PAGESIZE, PROT_WRITE | PROT_READ, MAP_SHARED, fd_shm, 0);
+	if(ptr == (void *)-1)
+		exit(1);
+
+  //lock is in the first entry in shared memory
 	mutex = (pthread_mutex_t*) ptr;
 
-
-    //setting up time variables for birth
+	//setting up time variables for birth
 	time_t t;
 	struct tm *now;
-	char time_buffer[25];
+	char created[25];
 	time (&t);
 	now = localtime(&t);
-	strftime(time_buffer, 25, "%c", now);
+	strftime(created, 25, "%c", now);
 
-    //time variables for elapsed time 
-	struct timeval start_time;
-	struct timeval curr_time;
+  //time variables for elapsed time 
+	struct timeval start_time, curr_time;
 	gettimeofday(&start_time, NULL);
-    int i;
+	
 	// critical section begins
 	pthread_mutex_lock(mutex);
 
-    //finding the first slot available in the shared memory
-    //and adding the client to this
-    //first slot is indicated by a pid of -1
-    //
-    //starting at 1 to skip over the lock
-	for (i = 1; i < 64; i++){
-		thisClient = (stats_t *) (ptr + 64 * i);
-		if (thisClient->pid == -1){
-            //found the first available slot
+  //finding the first slot available in the shared memory
+  //and adding the client to this
+  //first slot is indicated by a pid of -1
+  //
+  //starting at 1 to skip over the lock
+	for(int i = 1; i < MAXCLIENTS; i++){
+		thisClient = (stats_t *) (ptr + MAXCLIENTS * i);
+		if(thisClient->valid == 0){
+      //found the first available slot
 			thisClient->pid = getpid();
-			strcpy(thisClient->birth, time_buffer);
-			strcpy(thisClient->clientString, argv[1]);
 			thisClient->elapsed_sec = 0;
 			thisClient->elapsed_msec = 0;
-            //do not want to continue in loop so break
+			strcpy(thisClient->birth, created);
+			strcpy(thisClient->clientString, argv[1]);
+			thisClient->valid = 1;
+      //do not want to continue in loop so break
 			break;
 		}
-
+		if(i >= 63){
+			pthread_mutex_unlock(mutex);
+      exit(1);
+		}
 	}
-	// client updates available segment
 
-	pthread_mutex_unlock(mutex);
 	// critical section ends
+	pthread_mutex_unlock(mutex);
 
-    //max clients has been reached so no more clients allowed so exit
-    if(i > 63)
-        exit(1);
-
-
-	while (1) {
-        //updating elapesed time for this process
-        gettimeofday(&curr_time, NULL);
+	while(1){
+    //updating elapesed time for this process
+    gettimeofday(&curr_time, NULL);
+		
 		thisClient->elapsed_sec = (curr_time.tv_sec - start_time.tv_sec);
-		thisClient->elapsed_msec = (curr_time.tv_usec - start_time.tv_usec) / 100.0;
+		thisClient->elapsed_msec = (curr_time.tv_usec - start_time.tv_usec) / 1000.0;
+		
 		sleep(1);
-
+		
 		// Print active clients
 		printf("Active clients : ");
 		// critical section begins
-
 		pthread_mutex_lock(mutex);
-		for (int i = 1; i < 64; i++){
-			stats_t* currClient = (stats_t *) (ptr + 64 * i);
-			if(currClient->pid != -1){
-				printf("%d ", currClient->pid);
-				
-			}
 
+		for(int i = 1; i < MAXCLIENTS; i++){
+			stats_t* currClient = (ptr+MAXCLIENTS*i);
+			if(currClient->valid != 0){
+				printf("%d ", currClient->pid);
+			}
 		}
 		printf("\n");
-		pthread_mutex_unlock(mutex);
+
 		// critical section ends
+		pthread_mutex_unlock(mutex);
 	}
 
 	return 0;
--- Project3/3a/shm_server.c
+++ Project3/3a/shm_server.c
@@ -7,13 +7,19 @@
 #include <signal.h>
 #include <string.h>
 
+#define SHMNAME "petti_klisch"
+#define PAGESIZE 4096
+#define MAXCLIENTS 64
+
 // Mutex variables
 pthread_mutex_t* mutex;
 pthread_mutexattr_t mutexAttribute;
+
 //pointer for the shared memory
-void * ptr;
+void* ptr;
 
 typedef struct {
+	char valid;
 	int pid;
 	char birth[25];
 	char clientString[10];
@@ -23,10 +29,10 @@ typedef struct {
 
 void exit_handler(int sig)
 {
-	if(munmap(ptr, 4096) != 0)
-		exit(1);
+	if(shm_unlink(SHMNAME) != 0)
+		exit(1);	
 
-	if(shm_unlink("petti_klisch") != 0)
+	if(munmap(ptr, PAGESIZE) != 0)
 		exit(1);
 
 	exit(0);
@@ -34,10 +40,10 @@ void exit_handler(int sig)
 
 int main(int argc, char *argv[])
 {
-
-	if (argc > 1)
+	if(argc > 1)
 		exit(1);
-    //setting up sighandler
+  
+	//setting up sighandler
 	struct sigaction act;
 	memset(&act, 0, sizeof(act));
 	act.sa_handler = exit_handler;
@@ -48,20 +54,23 @@ int main(int argc, char *argv[])
 
 	if(sigaction(SIGINT, &act, NULL) != 0)
 		exit(1);
+	
 	// Creating a new shared memory segment
-	int fd_shm = shm_open("petti_klisch", O_RDWR | O_CREAT, 0660);
-	if (fd_shm == -1)
+	int fd_shm = shm_open(SHMNAME,  O_CREAT|O_RDWR, 0660);
+	if(fd_shm == -1)
 		exit(1);
-    //truncating the shared memory to one page size
-	int t = ftruncate(fd_shm, 4096);
-	if (t == -1)
+  
+	//truncating the shared memory to one page size
+	int t = ftruncate(fd_shm, PAGESIZE);
+	if(t == -1)
 		exit(1);
-    //setting up pointer
-	ptr = mmap(NULL, 4096, PROT_WRITE | PROT_READ, MAP_SHARED, fd_shm, 0);
-	if (ptr == NULL)
+  
+	//setting up pointer
+	ptr = mmap(NULL, PAGESIZE, PROT_READ|PROT_WRITE, MAP_SHARED, fd_shm, 0);
+	if(ptr == NULL)
 		exit(1);
 
-    //lock is the first entry in the shared memory
+  //lock is the first entry in the shared memory
 	mutex = (pthread_mutex_t*) ptr;
 
 	// Initializing mutex
@@ -69,37 +78,22 @@ int main(int argc, char *argv[])
 	pthread_mutexattr_setpshared(&mutexAttribute, PTHREAD_PROCESS_SHARED);
 	pthread_mutex_init(mutex, &mutexAttribute);
 
-	
-
-    //setting all the pids to -1 initially to indicate that the memory is available
-    //to any client
-    //staring at i = 1 to skip over the lock
-	for (int i = 1; i < 64; i++){
-		stats_t* client = (stats_t *) (ptr + 64 * i);
-		client->pid = -1;
-	}
-    //iteration counter for printing
+  //iteration counter for printing
 	int it = 1;
-	while (1) {
-		// ADD
 
+	while(1){
 		sleep(1);
-        //printing out information of all the clients
-		for (int i = 1; i < 64; i++){
-			stats_t* currClient = (stats_t *) (ptr + 64 * i);
-			if (currClient -> pid != -1) {
-				
-				printf("%d, pid : %d, ", it, currClient -> pid);
-				printf("birth : %s, ", currClient -> birth);
-				printf("elapsed : %d s %.4f ms, %s\n", currClient -> elapsed_sec, currClient -> elapsed_msec, currClient -> clientString);
-				
+    //printing out information of all the clients
+		for(int i = 1; i < MAXCLIENTS; i++){
+			stats_t* currClient = (ptr+MAXCLIENTS*i);
+			if(currClient->valid != 0){
+				printf("%d, pid : %d, ", it, currClient->pid);
+				printf("birth : %s, ", currClient->birth);
+				printf("elapsed : %d s %.4f ms, %s\n", currClient->elapsed_sec, 
+					currClient->elapsed_msec, currClient->clientString);				
 			}
 		}
-
 		it++;
-
-
-
 	}
 
 	return 0;
